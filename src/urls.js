e88a3201cc00270126e49bee277a2f9a900a48b2
Ekene Attoh <ekeneattoh@Ekenes-Air.lan>
2019-08-22 21:11:29 +0200

    typo correction

--- src/urls.js
+++ src/urls.js
@@ -1,3 +1,3 @@
 //holds all url needed by the app
 
-export const ncheta_backend = "https://nchetaapi.herokuapp.com/"; //heroku back end
+export const ncheta_backend = "https://nchetaapi.herokuapp.com"; //heroku back end
