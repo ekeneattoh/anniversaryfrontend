63d894325f56e39b1df9a926ee90b36d28952077
ekeneattoh <ekeneattoh@gmail.com>
2019-11-16 23:59:44 +0100

    typo fix

--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,4 @@
 //holds all url needed by the app
 
-export const ncheta_backend = "https://nchetabackend.herokuapp.com/"; //heroku back end
+export const ncheta_backend = "https://nchetabackend.herokuapp.com"; //heroku back end
 // export const ncheta_backend = "http://localhost:9000"; //heroku back end
