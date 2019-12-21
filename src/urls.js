073aa19c17f57acc37308ea124a0ba3736beb950
Ekene Attoh <ekeneattoh@Ekenes-Air.lan>
2019-12-21 19:17:40 +0100

    bug fix

--- src/urls.js
+++ src/urls.js
@@ -4,7 +4,7 @@ let NCHETA_BACKEND_URL = process.env.REACT_APP_NCHETA_DEV_BACKEND_URL
 
 if (process.env.NODE_ENV === 'production') {
 
-    NCHETA_BACKEND_URL = REACT_APP_NCHETA_PRD_BACKEND_URL
+    NCHETA_BACKEND_URL = process.env.REACT_APP_NCHETA_PRD_BACKEND_URL
 }
 
 export const ncheta_backend = NCHETA_BACKEND_URL;
